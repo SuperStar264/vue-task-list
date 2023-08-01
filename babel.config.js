67f1254edbf3031ef450580c4c028b2d1d05dd6f
SuperStar264 <bz3795266@gmail.com>
2023-08-01 17:11:15 +0800

    fix babel

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
-module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+// module.exports = {
+//   presets: [
+//     '@vue/cli-plugin-babel/preset'
+//   ]
+// }
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/vue-task-list/" : "/",
 };
